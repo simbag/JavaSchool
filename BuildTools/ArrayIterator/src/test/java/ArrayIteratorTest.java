ea0f32de9094ca78983ad8c5dc9a49f43805a87c
simbag <simbag1@yandex.ru>
2017-09-14 07:28:36 +0300

    fix test

--- BuildTools/ArrayIterator/src/test/java/ArrayIteratorTest.java
+++ BuildTools/ArrayIterator/src/test/java/ArrayIteratorTest.java
@@ -8,36 +8,36 @@ import static org.junit.Assert.assertEquals;
 public class ArrayIteratorTest {
 
     private String[] array;
-    private ArrayIterator<String> integerArrayIterator;
+    private ArrayIterator<String> stringArrayIterator;
 
 
     public ArrayIteratorTest() {
         this.array = new String[]{"1","2","3","4","5"};
-        this.integerArrayIterator = new ArrayIterator<>(this.array);
+        this.stringArrayIterator = new ArrayIterator<>(this.array);
     }
 
     @Test
     public void nextTest(){
-        assertEquals("1",integerArrayIterator.next());
-        assertEquals("2",integerArrayIterator.next());
-        assertEquals("3",integerArrayIterator.next());
-        assertEquals("4",integerArrayIterator.next());
-        assertEquals("5",integerArrayIterator.next());
+        assertEquals("1",stringArrayIterator.next());
+        assertEquals("2",stringArrayIterator.next());
+        assertEquals("3",stringArrayIterator.next());
+        assertEquals("4",stringArrayIterator.next());
+        assertEquals("5",stringArrayIterator.next());
     }
 
     @Test(expected = NoSuchElementException.class)
     public void nextTestException(){
-        integerArrayIterator.next();
-        integerArrayIterator.next();
-        integerArrayIterator.next();
-        integerArrayIterator.next();
-        integerArrayIterator.next();
-        integerArrayIterator.next();
+        stringArrayIterator.next();
+        stringArrayIterator.next();
+        stringArrayIterator.next();
+        stringArrayIterator.next();
+        stringArrayIterator.next();
+        stringArrayIterator.next();
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void nextRemoveException(){
-        integerArrayIterator.remove();
+        stringArrayIterator.remove();
     }
 
 }
